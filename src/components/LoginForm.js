635103383a70f897e62a51a2844a9caca0e45fdc
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2023-01-07 00:57:30 +0200

    Edit: change the how to handle the login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { TextField, Button } from "@material-ui/core";
+import { React, useState } from "react";
+import { TextField, Button, Alert } from "@material-ui/core";
 import { auth } from "../utils/firebase"; // Import the Firebase auth object
 
+import "./LoginForm.css"; // Import the LoginForm.css file
+
 function LoginForm() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -15,22 +17,46 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <TextField
-        label="Email"
-        type="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <TextField
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      {error && <p>{error}</p>}
-      <Button type="submit">Log in</Button>
-    </form>
+    // <form onSubmit={handleLogin} className="login-form fade-in">
+    //   <TextField
+    //     label="Email"
+    //     type="email"
+    //     value={email}
+    //     onChange={(event) => setEmail(event.target.value)}
+    //   />
+    //   <TextField
+    //     label="Password"
+    //     type="password"
+    //     value={password}
+    //     onChange={(event) => setPassword(event.target.value)}
+    //   />
+    //   {error && <p>{error}</p>}
+    //   <Button type="submit">Log in</Button>
+    // </form>
+    <div className="page-wrapper">
+      <form className="login-form" onSubmit={handleLogin}>
+        <input
+          className="form-control"
+          placeholder="Email"
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          className="form-control"
+          placeholder="Password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        {error && <p>{error}</p>}
+        <button className="btn" type="submit">
+          Log In
+        </button>
+      </form>
+    </div>
   );
 }
 
